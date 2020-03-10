3881ceb0b8d99d89f112fd55b40b0c560788e73e
margarita malnik <rita46297@gmail.com>
2020-03-10 18:50:15 +0200

    change

---
+++ SocialIoTests/SocialIoTests 2.swift
@@ -0,0 +1,34 @@
+//
+//  SocialIoTests.swift
+//  SocialIoTests
+//
+//  Created by Margarita Malnik on 06/03/2020.
+//  Copyright © 2020 Margarita Malnik. All rights reserved.
+//
+
+import XCTest
+@testable import SocialIo
+
+class SocialIoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
